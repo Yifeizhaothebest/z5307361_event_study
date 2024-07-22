cb0aed5bb3ec351463cd73849c5306f19e0aa43c
z5307361YifeiZhao <yifeizhaothebest@gmail.com>
2024-07-22 18:14:18 +1000

    This is week 1

---
+++ _backup/2024-07-17_12_17_38/week1.py
@@ -0,0 +1,12 @@
+print("Welcome to the Toolkit for Finance T2 2024, hello from Yifei Zhao")
+
+
+
+d = { 'a': 'b', 'c': 'd'}
+
+for key,value in d.items():
+    f= f'{key} : {value}'
+
+a = 'aaaa'
+
+print(a.split('='))
